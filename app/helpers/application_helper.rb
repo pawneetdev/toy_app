28d43f17619b4bfeb3937ea1213e8ed79a8c4e71
Pawneet Singh <pawneet.dev@gmail.com>
2016-07-25 20:44:19 +0530

    Bootstrap, styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def full_title(page_title = '')
+		base_title = "My app"
+
+		if page_title.empty?
+			title = base_title
+		else
+			title = base_title + "| " + page_title.to_s
+		end
+	end
 end
